305f7ff49426289a3d2e8213be18cc4d29b51e04
Diego <diegolara93345@gmail.com>
2024-03-13 21:28:17 -0500

    finished part 1 and 2, very bad and unorganized code for rn

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use std::fmt::Error;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::string;
 use std::vec;
 fn main() -> std::io::Result<()> {
     // EACH BINARY IS 12 NUMBERS LONG GAMMA MOST COMMON EPSILON NOT COMMON
@@ -20,7 +22,7 @@ fn main() -> std::io::Result<()> {
         let mut least_common_bit = String::new();
         for lines in contents.lines() {
             if &lines[i..i+1] == "1" { amnt_of_ones += 1}
-            else {amnt_of_zeroes +=1 }
+            else { amnt_of_zeroes +=1 }
         }
         if amnt_of_ones > amnt_of_zeroes { 
             common_bit = String::from("1");
@@ -35,6 +37,7 @@ fn main() -> std::io::Result<()> {
             temp_epsilon.push(least_common_bit);
         }
     }
+    println!("{:?}", temp_epsilon);
     let new = temp_epsilon.join("");
     let new2 = temp_gamma.join("");
     epsilon_rate.push(new);
@@ -47,5 +50,70 @@ fn main() -> std::io::Result<()> {
     println!("{int_gamma_epsilon}");
     let total = int_gamma_epsilon * int_val_epsilon;
     println!("{total}");
+    let new1 =  part2(contents.clone(), 0);
+    let new2 = part2_2(contents.clone(), 0);
+    let new_int = isize::from_str_radix(new1.as_str(), 2).unwrap();
+    let new_int2 = isize::from_str_radix(new2.as_str(), 2).unwrap();
+    let total2 = new_int * new_int2;
+    println!("{total2}");
     Ok(())
+}
+
+fn part2(contents: String, mut index: i32) -> String {
+    let mut strings_vec: Vec<&str> = vec![];
+    let mut zeros = 0;
+    let mut ones = 0;
+    let mut leading = "0";
+    for lines in contents.lines() {
+        if lines[index as usize..(index as usize) +1 ] == "0".to_string() {
+            zeros += 1;
+        }
+        else { 
+            ones += 1;
+         }
+    }
+    if zeros > ones {  leading = "0";  }
+    else { leading = "1"; }
+    for lines in contents.lines() {
+        if &lines[index as usize..(index as usize) +1] == leading {
+            strings_vec.push(lines);
+        }
+    }
+    index += 1;
+    if index == 12 {
+        return strings_vec[0].to_string();
+    }
+    return part2(strings_vec.join("\n"), index);
+}
+
+fn part2_2(contents: String, mut index: i32) -> String {
+    let mut strings_vec: Vec<&str> = vec![];
+    let mut zeros = 0;
+    let mut ones = 0;
+    let mut leading = "0";
+    for lines in contents.lines() {
+        if lines[index as usize..index as usize +1] == "0".to_string() {
+            zeros += 1;
+        }
+        else { 
+            ones += 1;
+         }
+    }
+    if zeros <= ones { 
+         leading = "0"; 
+         }
+    else { 
+        leading = "1"; 
+    }
+    for lines in contents.lines() {
+        if &lines[index as usize..index as usize +1] == leading {
+            strings_vec.push(lines);
+        }
+    }
+    println!("{:?}", strings_vec);
+    index += 1;
+    if strings_vec.len() == 1 {
+        return strings_vec[0].to_string();
+    }
+    return part2_2(strings_vec.join("\n"), index);
 }
